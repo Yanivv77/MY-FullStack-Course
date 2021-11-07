715efe4ff0c030b74bd66d2e990488017e43970c
Yaniv Bialik <yanivv77@gmail.com>
2021-11-07 10:53:49 +0200

    Created Person Object

---
+++ C#/ConsoleApp1/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        string name = "";
+        string gender = "";
+       
+
+        public Person(string name, string gender)
+        {
+            this.name = name;
+            this.gender = gender;
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+    }
+}
---
+++ C#/ConsoleApp1/main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Person Avi = new Person("Avi", "Male");
+            
+        }
+    }
+}
