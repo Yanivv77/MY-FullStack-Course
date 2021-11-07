17c749b9c90422047bf494c6d75e6328acb60267
Yaniv Bialik <yanivv77@gmail.com>
2021-11-07 23:55:07 +0200

    folder fixed

---
+++ C#/Shapes/ShapeDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ShapeLib;
+
+Console.WriteLine("hello");
+var rect = new Rectangle();
+
+rect.Color = System.Drawing.Color.BlueViolet;
+
+var v = new Vector2d(1, 2);
+
+Console.ReadKey();
