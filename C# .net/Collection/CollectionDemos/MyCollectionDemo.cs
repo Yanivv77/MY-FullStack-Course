ed1b6ee5025bc7426edd2cd23f0e05b624d73012
Yaniv Bialik <yanivv77@gmail.com>
2021-11-11 14:09:45 +0200

    .

---
+++ C# .net/Collection/CollectionDemos/MyCollectionDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LinqDemo.Models;
+using Newtonsoft.Json;
+
+
+namespace CollectionDemos
+{
+    public class MyCollectionDemo
+    {
+        public static void Run()
+        {
+            Console.WriteLine("myCollectionDemo");
+
+            var exePath = Path.GetDirectoryName(typeof(MyCollectionDemo).Assembly.Location); //Path of the data file
+            var filename = Path.Combine(exePath, "Data", "MOCK_DATA.json"); // Load file
+            var text = File.ReadAllText(filename); // Read text
+            var people = JsonConvert.DeserializeObject<List<Person>>(text); // Convert text to object
+            var orderby = people.OrderBy(x => x.first_name);
+            var str = String.Concat(orderby.Select(o => o.ToString()));
+           
+
+
+
+
+
+            foreach (var p in orderby)
+            {
+                
+                Console.WriteLine(p);
+            }
+          
+
+        }
+
+       
+    }
+}
