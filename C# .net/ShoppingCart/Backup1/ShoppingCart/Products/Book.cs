ddb9b159b7030583a0ed7946b5d407331436ff62
Yaniv Bialik <54044065+Yanivv77@users.noreply.github.com>
2021-11-25 00:25:14 +0200

    Add files via upload

---
+++ C# .net/ShoppingCart/Backup1/ShoppingCart/Products/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Products
+{
+    class Book : Product
+    {
+        String Author {get; set; }
+        int PublishedYear {get; set; }
+
+        public Book(string author, int publishedYear)
+        {
+            Author = author;
+            PublishedYear = publishedYear;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString()+ Author + " " + PublishedYear;
+        }
+    }
+}
---
+++ C# .net/ShoppingCart/Backup1/ShoppingCart/Products/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace ShoppingCart.Products
+{
+    abstract class Product
+    {
+        protected int ProductID { get; set; } = 0;
+        protected string Name { get; set; }
+
+        public Product(int productID,string name)
+        {
+            ProductID = productID;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return "ID "+ ProductID + "Name " + Name;
+        }
+    }
+}
